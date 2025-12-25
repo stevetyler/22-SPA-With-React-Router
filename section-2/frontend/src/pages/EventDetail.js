998ab2137a366e14e5321da2460f7c816fe048d8
Steve Tyler <steve.tyler75@gmail.com>
2025-12-25 22:47:59 +0000

    section 383

--- section-2/frontend/src/pages/EventDetail.js
+++ section-2/frontend/src/pages/EventDetail.js
@@ -1,28 +1,64 @@
+import { Suspense } from "react";
 import EventItem from "../components/EventItem";  
-import { useRouteLoaderData, redirect } from "react-router-dom";
+import { useRouteLoaderData, redirect, Await } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const {event, events} = useRouteLoaderData('event-detail');
 
   return (
     <>  
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{textAlign: 'center'}}>Loading event...</p>}>   
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>  
+      </Suspense>
+      <Suspense fallback={<p style={{textAlign: 'center'}}>Loading events...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 }
 
 export default EventDetailPage; 
 
-export async function loader({ request, params }) {
-    // can't use useParams hook here, but can use ID
-    const id = params.eventId; 
-
-    const response = await fetch("http://localhost:8080/events/" + id);
+export async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
 
     if(!response.ok) {
       throw new Error({message: 'Could not fetch details'})
     } else {
-      return response;
+      const resData = await response.json();
+      return resData.event;
+    }
+}
+
+export async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ 
+        message: 'Could not fetch events.' }
+      ), { 
+        status: 500 
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+
+export async function loader({ request, params }) {
+    // can't use useParams hook here, but can use ID
+    const id = params.eventId; 
+
+    return {
+      event: await loadEvent(id), // waits for event to load before rendering route
+      events: loadEvents()
     }
 }
 
